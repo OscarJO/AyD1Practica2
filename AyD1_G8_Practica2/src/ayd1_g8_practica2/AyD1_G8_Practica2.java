cb7a90aff4290b0f4e4dc4f09be6336ea688d702
OscarJO <oscar.jordan.21091@gmail.com>
2020-09-17 18:55:33 -0600

    creación proyecto

---
+++ AyD1_G8_Practica2/src/ayd1_g8_practica2/AyD1_G8_Practica2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayd1_g8_practica2;
+
+/**
+ *
+ * @author Asus
+ */
+public class AyD1_G8_Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("prueba");
+    }
+    
+}
